e8730cf6e0bd4c05d97f8af12eab32001b70f34b
Web-CODED <progeng94@gmail.com>
2024-02-07 19:33:50 +0530

    Delete cdn.npfs.co directory

--- cdn.npfs.co/js/npf_tracker/google_analytics_universal.js
+++
@@ -1,9 +0,0 @@
-function google_analytics_universal_npf(tracking_id) {
-    window.dataLayer = window.dataLayer || [];
-
-    function gtag() {
-        dataLayer.push(arguments);
-    }
-    gtag('js', new Date());
-    gtag('config', tracking_id);
-}
